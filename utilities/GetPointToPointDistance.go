d27947a56aff470756086c76019c736ef30f8729
Ben Calnan <bencalnan@gmail.com>
2019-09-06 14:36:24 +0100

    Updates

--- utilities/GetPointToPointDistance.go
+++ utilities/GetPointToPointDistance.go
@@ -1,15 +1,20 @@
 package utilities
 
-import "math"
+import (
+	"math"
 
-//pointToPointDistanceCosine Get distance (in meters) bewteen two Lat/Longs using cosine formula. Has some benefits over using Haversine formula.
+	"github.com/bencalnan/geo-tools/core"
+)
+
+//PointToPointDistanceCosine Get distance (in meters) bewteen two Lat/Longs using cosine formula. Has some benefits over using Haversine formula.
 //Input and Output in Radians
-func pointToPointDistanceCosine(startPoint, endPoint Point) float64 {
-	var d = math.Acos(math.Sin(startPoint.Y)*math.Sin(endPoint.Y)+math.Cos(startPoint.Y)*math.Cos(endPoint.Y)*math.Cos(startPoint.X-endPoint.X)) * earthRadius
+func PointToPointDistanceCosine(startPoint, endPoint core.Point) float64 {
+	var d = math.Acos(math.Sin(startPoint.Y)*math.Sin(endPoint.Y)+math.Cos(startPoint.Y)*math.Cos(endPoint.Y)*math.Cos(startPoint.X-endPoint.X)) * EarthRadius
 	return d
 }
 
-func pointToPointHaversine(startLat, startLon, endLat, endLon float64) float64 {
+//PointToPointHaversine - Altnernative method for getting distance (in meters) bewteen two Lat/Longs using cosine formula.
+func PointToPointHaversine(startLat, startLon, endLat, endLon float64) float64 {
 
 	var startLatr = DegToRad(startLat)
 	var endLatR = DegToRad(endLat)
@@ -18,6 +23,12 @@ func pointToPointHaversine(startLat, startLon, endLat, endLon float64) float64 {
 
 	var a = math.Sin(difLat/2)*math.Sin(difLat/2) + math.Cos(startLatr)*math.Cos(endLatR)*math.Sin(difLon/2)*math.Sin(difLon/2)
 	var c = 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
-	var d = earthRadius * c
+	var d = EarthRadius * c
 	return d
+}
+
+//CartesianDistance - Get distance betweeen two cartesian points
+func CartesianDistance(source, target core.Point) float64 {
+	distance := math.Sqrt(math.Pow((target.X-source.X), 2) + math.Pow((target.Y-source.Y), 2))
+	return distance
 }
